aea7dba67d86502c3cb92ee01888c4b51327d571
DESKTOP-NUTUO9P\thoma <thoschulte@googlemail.com>
2021-04-28 00:21:25 +0200

    factory foo

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,36 +1,53 @@
 const customInterpolationApp = angular.module('customInterpolationApp', []);
 const url = 'https://localhost:8888/test';
 
-customInterpolationApp.config(function ($interpolateProvider) {
+customInterpolationApp.config($interpolateProvider => {
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
 });
 
-customInterpolationApp.controller('DemoController', ['$http', function ($scope) {
-    $scope.label = "This binding is brought you by [[]] interpolation symbols.";
+customInterpolationApp.factory('_cart', () => {
+    const items = [ {id: 1, name: 'Pizza Free', price: 7}];
 
-    //
-    // $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
-    // then(function(response) {
-    //     $scope.status = response.status;
-    //     $scope.data = response.data;
-    //
-    //     $scope.vorname = res[0].name;
-    //     $scope.nachname = res[0].surname;
-    // }, function(response) {
-    //     $scope.data = response.data || 'Request failed';
-    //     $scope.status = response.status;
-    // });
-    //
-    // fetch(url)
-    //     .then(function(res) {
-    //         return res.json();
-    //     })
-    //     .then(function(res) {
-    //         console.log(res[0].name);
-    //
-    //     })
-    //     .catch(function(err) {
-    //         console.error(err);
-    //     });
-}]);
+    return {
+        getItems: () => {
+            return items;
+        },
+        addArticle: (article) => {
+            items.push(article);
+            console.log(items);
+        },
+        sum: () => {
+            return items.reduce((total, article) => {
+                return total + article.price;
+            }, 0);
+        }
+    };
+});
+
+customInterpolationApp.controller('DemoCtl', ($scope, $http, _cart) => {
+    $scope.cart = _cart;
+    $scope.label = "This binding is brought you by [[]] interpolation symbols.";
+    $scope.vorname = "Thomas";
+    $scope.nachname = "Schulte";
+    $scope.pizzas = [
+        {id: 1, name: 'Pizza Dutchman', price: 5},
+        {id: 2, name: "Pizza Hot Salami", price: 5.5},
+        {id: 3, name: "Pizza Vegan", price: 6},
+        {id: 4, name: "Pizza Vegetaria", price: 5},
+        {id: 5, name: "Pizza Salami", price: 5.5},
+        {id: 6, name: "Pizza Thunfisch", price: 6}
+    ];
+
+    $http.get('https://localhost:8888/test')
+        .then((articlesResponse) => {
+            $scope.articles = articlesResponse.data;
+            console.log(_cart.getItems());
+        })
+        .catch(e => {
+            console.error(e);
+            alert(e);
+        });
+});
+
+
