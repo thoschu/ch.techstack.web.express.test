ffd0a015cac7fa7f4e9d7250f6160ab71a444aeb
DESKTOP-NUTUO9P\thoma <thoschulte@googlemail.com>
2021-05-14 20:15:44 +0200

    swagger openapi done

--- app/routers/test.router.js
+++ app/routers/test.router.js
@@ -3,82 +3,214 @@ const router = express.Router();
 const {testAction, testDeleteAction} = require('../controllers');
 
 /**
- *  @swagger
- *  definitions:
- *      test:
- *          properties:
- *              id:
- *                  type: number
- *                  format: int64
- *              title:
+ *  @openapi
+ *  components:
+ *      headers:
+ *          Location:
+ *              description: URI der neu erzeugten Ressource
+ *              schema:
  *                  type: string
- *              active:
- *                  type: boolean
+ *                  format: uri
+ *      parameters:
+ *          Mid:
+ *              name: mid
+ *              in: path
+ *              required: true
+ *              schema:
+ *                  type: integer
+ *              description: Id des Modells
+ *          Wid:
+ *              name: wid
+ *              in: path
+ *              required: true
+ *              schema:
+ *                  type: integer
+ *                  description: Id des Werks
+ *          Limit:
+ *              name: limit
+ *              in: query
+ *              required: false
+ *              schema:
+ *                  type: integer
+ *                  format: int64
+ *                  minimum: 1
+ *      responses:
+ *          Modell:
+ *              description: OK
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Modell'
+ *              links:
+ *                  Werk:
+ *                      operationId: /test/werke/{wid}/
+ *                      parameters:
+ *                          wid: $response.body#/werk
+ *          Werk:
+ *              description: OK
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Werk'
+ *      requestBodies:
+ *          Modell:
+ *              description: Modell post request body example
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Modell'
+ *      schemas:
+ *          Modell:
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                      format: int64
+ *                      example: 1377
+ *                  name:
+ *                      type: string
+ *                      maxLength: 20
+ *                      example: Modell 7
+ *                  werk:
+ *                      type: integer
+ *                      example: 325
+ *          ModellItem:
+ *              type: object
+ *              properties:
+ *                  name:
+ *                      type: string
+ *                  self_uri:
+ *                      type: string
+ *                      format: uri
+ *              example:
+ *                  name: ID.3
+ *                  self_uri: /modell/77
+ *          Werk:
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                      format: int64
+ *                      example: 1977
+ *                  name:
+ *                      type: string
+ *                      maxLength: 10
+ *                      example: Hamburg
+ *      securitySchemes:
+ *          prod:
+ *              scheme: bearer
+ *              type: http
+ *              bearerFormat: jwt
+ *          keycloak:
+ *              type: oauth2
+ *              flows:
+ *                  implicit:
+ *                      authorizationUrl: https://iam-dev.ingress1.dev-lbobka.de-1.mk.psmanaged.com/auth/realms/cx-portal/protocol/openid-connect/auth
+ *                      scopes: {}
  */
 
 /**
- *  @swagger
- *  /test:
- *      get:
- *          security:
- *              - bearerAuth: []
+ *  @openapi
+ *  /test/:
+ *      post:
  *          tags:
- *              - tests
- *          description: Returns all tests
- *          summary: Foo bar baz
- *          produces:
- *              - application/json
- *              - application/xml
+ *              - Test
+ *          summary: Modell erzeugen
+ *          description: |
+ *              ![ts icon](https://www.thomas-schulte.de/html/images/favicon.ico)
+ *
+ *              Erzeugt ein **neues** Modell
+ *
+ *              |A|B|C|
+ *              |---|---|---|
+ *              |1|2|3|
+ *          requestBody:
+ *              $ref: '#/components/requestBodies/Modell'
+ *          responses:
+ *              201:
+ *                  description: Created
+ *                  headers:
+ *                      Location:
+ *                          $ref: '#/components/headers/Location'
+ *          security:
+ *              - keycloak: []
+ *      get:
+ *          tags:
+ *              - Test
+ *          summary: Modelle wiedergeben
+ *          description: Gibt alle Modelle als Liste zurück
+ *          parameters:
+ *              - $ref: '#/components/parameters/Limit'
  *          responses:
  *              200:
- *                  description: An array of test datasets
- *                  schema:
- *                      type: object
- *                      properties:
- *                          id:
- *                              type: integer
- *                              description: The user ID.
- *                          username:
- *                              type: string
- *                              description: The user name.
- *              400:
- *                  description: Bad request. User ID must be an integer and bigger than 0.
+ *                  description: OK
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: array
+ *                              items:
+ *                                  $ref: '#/components/schemas/ModellItem'
  *              401:
- *                  description: Authorization information is missing or invalid.
- *              404:
- *                  description: A user with the specified ID was not found.
+ *                  description: Not authenticated
+ *              403:
+ *                  description: Access token does not have the required scope
  */
-
 router.get('/', testAction);
+router.post('/', testAction);
 
 /**
- * @swagger
- * /users:
- *   get:
- *     summary: Retrieve a list of JSONPlaceholder users.
- *     description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
- *     responses:
- *       200:
- *         description: A list of users.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: array
- *                   items:
- *                     type: object
- *                     properties:
- *                       id:
- *                         type: integer
- *                         description: The user ID.
- *                         example: 0
- *                       name:
- *                         type: string
- *                         description: The user's name.
- *                         example: Leanne Graham
+ *  @openapi
+ *  /test/{mid}/:
+ *      parameters:
+ *          - $ref: '#/components/parameters/Mid'
+ *      get:
+ *          tags:
+ *              - Test
+ *          summary: Modell wiedergeben nach Modell id
+ *          description: Gibt ein Modell gem. Modell-Id zurück
+ *          responses:
+ *              200:
+ *                  $ref: '#/components/responses/Modell'
+ *      put:
+ *          tags:
+ *              - Test
+ *          summary: Modell verändern
+ *          description: Verändert ein Modell gem. Modell-Id
+ *          requestBody:
+ *              $ref: '#/components/requestBodies/Modell'
+ *          responses:
+ *              200:
+ *                  $ref: '#/components/responses/Modell'
+ *      delete:
+ *          tags:
+ *              - Test
+ *          summary: Modell löschen nach modell id
+ *          description: Löscht ein Modell gem. Modell-Id zurück
+ *          responses:
+ *              200:
+ *                  description: OK
  */
+router.get('/:mid', testAction);
+router.put('/:mid', testAction);
+router.delete('/:mid', testAction);
+
+/**
+ *  @openapi
+ *  /test/werke/{wid}/:
+ *      parameters:
+ *          - $ref: '#/components/parameters/Wid'
+ *      get:
+ *          tags:
+ *              - Werk
+ *          summary: Foo
+ *          description: bar baz
+ *          operationId: /test/werke/{wid}/
+ *          responses:
+ *              200:
+ *                  $ref: '#/components/responses/Werk'
+ */
+router.get('werke/:wid', testAction);
+
 router.get('/delete/:id?', testDeleteAction);
 
 module.exports = router;
--- swagger.js
+++ swagger.js
@@ -3,25 +3,28 @@ const swaggerJSDoc = require('swagger-jsdoc');
 console.info('https://dev.to/kabartolo/how-to-document-an-express-api-with-swagger-ui-and-jsdoc-50do');
 
 const swaggerDefinition = {
+    openapi: '3.0.0',
     info: {
         title: 'Test API',
         version: '1.0.0',
         description: 'API for testing purposes',
         license: {
             name: 'Licensed Under MIT',
-            url: 'https://spdx.org/licenses/MIT.html',
+            url: 'https://opensource.org/licenses/MIT',
         },
         contact: {
-            name: 'JSONPlaceholder',
-            url: 'https://jsonplaceholder.typicode.com',
+            name: 'Tom S.',
+            url: 'https://www.thomas-schulte.de/disclaimer.html',
         }
     },
-    openapi: '3.0.0',
     servers: [
         {
             url: 'https://localhost:8888',
-            description: 'Development server',
-        },
+            description: 'Development server 1',
+        }, {
+            url: 'https://localhost:8899',
+            description: 'Development server 2',
+        }
     ],
     host: 'localhost:8888',
     basePath: '/'
@@ -32,6 +35,6 @@ const options = {
     apis: [
         './app/routers/test.router.js'
     ]
-}
+};
 
 module.exports = swaggerJSDoc(options);
